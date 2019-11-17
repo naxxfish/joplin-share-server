89fc7b54611fbaac199b14c5789dfd86f5d57e3c
Chris Roberts <chris@naxxfish.net>
2019-11-17 14:50:45 +0000

    Add helmet, compression - also use the correct env var for port
    Signed-off-by: Chris Roberts <chris@naxxfish.net>

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const expressWinston = require('express-winston');
 const winston = require('winston');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(compression());
+app.use(helmet());
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000,
+	max: 5,
+});
+app.use(limiter);
 
 app.use(expressWinston.logger({
 	transports: [
@@ -20,7 +31,7 @@ app.use(expressWinston.logger({
 app.use(express.static(`${__dirname}/public`));
 app.use(require('./controllers/'));
 
-const port = process.env['port'] || 3000;
+const port = process.env['PORT'] || 3000;
 const server = app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
