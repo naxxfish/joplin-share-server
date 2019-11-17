8cc1628914ed386c85b93a1d500b30c26eea6d61
Chris Roberts <chris@naxxfish.net>
2019-11-17 14:33:50 +0000

    add install script

---
+++ src/util/install.js
@@ -0,0 +1,24 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
+const client = new pg.Client(connectionString);
+const createQueryPromises = [];
+createQueryPromises.push(
+	client.query(`CREATE TABLE "public"."notes" (
+		"note_data" text NOT NULL,
+		"note_encryption_type" text NOT NULL,
+		"note_originator" text NOT NULL,
+		"note_date_created" timestamp NOT NULL,
+		"note_id" text NOT NULL,
+		"note_version" bigint NOT NULL,
+		CONSTRAINT "notes_note_id" PRIMARY KEY ("note_id")
+	) WITH (oids = false);`));
+Promise.all(createQueryPromises)
+	.then(() => {
+		console.log('Database initialised!');
+		process.exit(0);
+	})
+	.catch((error) => {
+		console.error(`Error installing database: ${error}`);
+		process.exit(1);
+	});
