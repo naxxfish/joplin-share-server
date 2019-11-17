2bf614c9c58daf6dda521a42abe2243335b1ddaa
Chris Roberts <chris@naxxfish.net>
2019-11-17 15:25:05 +0000

    That's not how the pg.Client API works...

--- src/util/install.js
+++ src/util/install.js
@@ -14,25 +14,30 @@ try {
 	console.error(`Couldn't make PostgreSQL client: ${e}`);
 	process.exit(1);
 }
-
-const createQueryPromises = [];
-createQueryPromises.push(
-	client.query(`CREATE TABLE "notes" (
-		"note_data" text NOT NULL,
-		"note_encryption_type" text NOT NULL,
-		"note_originator" text NOT NULL,
-		"note_date_created" timestamp NOT NULL,
-		"note_id" text NOT NULL,
-		"note_version" bigint NOT NULL,
-		CONSTRAINT "notes_note_id" PRIMARY KEY ("note_id")
-	) WITH (oids = false);`));
-Promise.all(createQueryPromises)
-	.then((results) => {
-		console.log(results);
-		console.log('Database initialised!');
-		process.exit(0);
-	})
-	.catch((error) => {
-		console.error(`Error installing database: ${error}`);
+client.connect((error) => {
+	if (error) {
+		console.error(`Error connecting to PostgreSQL: ${error.stack}`);
 		process.exit(1);
-	});
+	}
+	const createQueryPromises = [];
+	createQueryPromises.push(
+		client.query(`CREATE TABLE "notes" (
+			"note_data" text NOT NULL,
+			"note_encryption_type" text NOT NULL,
+			"note_originator" text NOT NULL,
+			"note_date_created" timestamp NOT NULL,
+			"note_id" text NOT NULL,
+			"note_version" bigint NOT NULL,
+			CONSTRAINT "notes_note_id" PRIMARY KEY ("note_id")
+		) WITH (oids = false);`));
+	Promise.all(createQueryPromises)
+		.then((results) => {
+			console.log(results);
+			console.log('Database initialised!');
+			process.exit(0);
+		})
+		.catch((error) => {
+			console.error(`Error installing database: ${error}`);
+			process.exit(1);
+		});
+});
